3b039caf85bd8125962edc9c06b3834b014f1334
Joshua Ochalek <jo447@cornell.edu>
2024-03-28 21:04:33 -0400

    Fix graph output
    - Adjusted boundaries to be in scope of data

--- INSTALL.md
+++ INSTALL.md
@@ -1,5 +1,5 @@
 # Installation
-Use the following command to install the required libraries for this application:
+Use the following commands to install the required libraries for this application:
 
 ```bash
 opam install cohttp-lwt-unix cohttp-async plplot
--- lib/plot.ml
+++ lib/plot.ml
@@ -1,9 +1,11 @@
 open Plplot
 
 let make_plot x y =
+  let y_min = Array.fold_left (fun curr_min el -> min curr_min el) y.(0) y in
+  let y_max = Array.fold_left (fun curr_min el -> max curr_min el) y.(0) y in
   plinit ();
   plcol0 2; 
-  plenv 0.0 6.0 0.0 30.0 0 0; (* TODO: update boundaries based on arrays *)
+  plenv 0.0 (float_of_int ((Array.length x) + 2)) (y_min -. 10.) (y_max +. 10.) 0 0;
   plcol0 3;
   plline x y;
   plend ();
