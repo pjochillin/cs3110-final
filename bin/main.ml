7437c091e9fb269d5b58ee8127ae6edbb01773db
Krish Mehra <km937@cornell.edu>
2024-05-06 13:43:50 -0400

    resolve git conflicts

--- bin/main.ml
+++ bin/main.ml
@@ -20,27 +20,6 @@ let parse_json_to_tables json =
   in
   make_table [] [] [] [] [] 0. data
 
-<<<<<<< HEAD
-let main () =
-  if Array.length Sys.argv < 2 then
-    failwith "Must provide a ticker as an argument!";
-  let ticker = Sys.argv.(1) in
-  let series = Api.time_series ticker in
-  let json = Api.assoc_of_json series in
-  (* let x, open_data, high_data, low_data, close_data =
-     parse_json_to_tables json *)
-  let open_data = Api.opens json in
-  let high_data = Api.highs json in
-  let low_data = Api.lows json in
-  let close_data = Api.closes json in
-  let x = Api.range_x (Array.length open_data) in
-  let rsi_values = Analysis.rsi (Array.to_list close_data) 14 in
-  Printf.printf "[%a]\n"
-    (fun ppf -> List.iter (Printf.fprintf ppf "%.2f; "))
-    rsi_values;
-  Final.Plot.make_plot x open_data high_data low_data close_data;
-  Lwt.return_unit
-=======
 (* let main () =
    if Array.length Sys.argv < 2 then
      failwith "Must provide a ticker as an argument!";
@@ -64,6 +43,5 @@ let main () =
      signal_line;
    Final.Plot.make_plot x open_data high_data low_data close_data;
    Lwt.return_unit *)
->>>>>>> ac9e049 (Add controller.ml)
 
 (* let () = Lwt_main.run (main ()) *)
