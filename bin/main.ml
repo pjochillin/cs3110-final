c03237b065ea70cc30ebf37f17c33fb7b4c82640
Joshua Ochalek <jo447@cornell.edu>
2024-05-10 00:16:36 -0400

    Add graphs to GUI
    - Allow ticker input on GUI
    - Change graph after change in text input + button press

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,10 @@
 open Final
 open Bogue
 
-let main () =
-   if Array.length Sys.argv < 2 then
-     failwith "Must provide a ticker as an argument!";
-   let ticker = Sys.argv.(1) in
+let main ticker =
+   (* if Array.length Sys.argv < 2 then
+     failwith "Must provide a ticker as an argument!"; *)
+   (* let ticker = Sys.argv.(1) in *)
    let series = Api.time_series ticker in
    let json = Api.assoc_of_json series in
    let open_data = Api.opens json in
@@ -12,7 +12,7 @@ let main () =
    let low_data = Api.lows json in
    let close_data = Api.closes json in
    let x = Api.range_x (Array.length open_data) in
-   let rsi_values = Final.Analysis.rsi (Array.to_list close_data) 14 in
+   (* let rsi_values = Final.Analysis.rsi (Array.to_list close_data) 14 in
    Printf.printf "[%a]\n"
      (fun ppf -> List.iter (Printf.fprintf ppf "%.2f; "))
      rsi_values;
@@ -23,22 +23,38 @@ let main () =
    let _, signal_line = Final.Analysis.macd (Array.to_list close_data) in
    Printf.printf "[%a,]\n"
      (fun ppf -> List.iter (Printf.fprintf ppf "%.2f; "))
-     signal_line;
+     signal_line; *)
    Plot.make_plot x open_data high_data low_data close_data;
    Lwt.return_unit
 
 (* let () = Lwt_main.run (main ()) *)
 
+let ticker_input = Widget.text_input ()
+
 let () =
   let top = [Widget.empty 120 0 (); Widget.label "OCaml Financial Analysis" ?size:(Some 64) ?align:(Some Center)]
     |> Layout.flat_of_w in
   let ticker_text = Widget.label "Enter a ticker below:" ?size:(Some 20) in
-  let ticker_input = Widget.text_input () in
   let button_text = Label.create ?size:(Some 15) ?align:(Some Center) "Get Data" in
   let ticker_button = Widget.button ?label:(Some button_text) "Get Data" in
   let button_row = [ticker_input; ticker_button]
     |> Layout.flat_of_w in
-  [top; Layout.resident ticker_text; button_row; Layout.resident (Widget.empty 1000 600 ())]
-    |> Layout.tower
+  let layout = [top; Layout.resident ticker_text; button_row; Layout.resident (Widget.empty 1000 600 ())]
+    |> Layout.tower in
+  let rec button_fun _ = 
+    let () = Lwt_main.run (main (Widget.get_text ticker_input)) in
+    let graph = Widget.image ~w:1000 ~h:500 "plot.jpeg" in
+    let top = [Widget.empty 120 0 (); Widget.label "OCaml Financial Analysis" ?size:(Some 64) ?align:(Some Center)]
+      |> Layout.flat_of_w in
+    let button_text = Label.create ?size:(Some 15) ?align:(Some Center) "Get Data" in
+    let ticker_button = Widget.button ?label:(Some button_text) "Get Data" in
+    let button_row = [ticker_input; ticker_button]
+      |> Layout.flat_of_w in
+    let tower = [top; Layout.resident ticker_text; button_row; Layout.resident graph]
+      |> Layout.tower in
+    Layout.set_rooms layout [tower]; 
+    Widget.on_click button_fun ticker_button in
+  Widget.on_click button_fun ticker_button;
+  layout
     |> Bogue.of_layout
     |> Bogue.run
